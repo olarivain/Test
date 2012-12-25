fb999337598a0ce03d15c6d665564bbd02f421be
kra <olarivain@gmail.com>
2012-12-24 17:18:07 -0800

    - fixed issue where borders were being drawn off view, causing weird aliasing issues

--- Test/KRARoundedCorners.m
+++ Test/KRARoundedCorners.m
@@ -39,7 +39,7 @@
     self.backgroundColor = [UIColor clearColor];
     
     self.borderColor = [UIColor blackColor];
-    self.borderWidth = 1.0;
+    self.borderWidth = 1.0f;
     self.cornerRadius = 5.0f;
     self.corners = UIRectCornerAllCorners;
 }
@@ -67,19 +67,27 @@
 
 
 #pragma mark - draw routine
-- (void)drawRect:(CGRect)rect
-{
+- (void)drawRect:(CGRect)rect {
     [super drawRect: rect];
+ 
+    // Since CoreGraphics draws around the origin rather than inside it,
+    // we have to inset the rounded rect by half the border width to avoid the edges
+    // being cut in half and making the corner look like crap
+    rect = CGRectInset(rect, _borderWidth/2.0f, _borderWidth/2.0f);
     
+    // grab the context and copy over the drawing attributes
     CGContextRef context = UIGraphicsGetCurrentContext();
     CGContextSetStrokeColorWithColor(context, self.borderColor.CGColor);
     CGContextSetLineWidth(context, self.borderWidth);
     CGContextSetFillColorWithColor(context, self.fillColor.CGColor);
-    CGContextSetShouldAntialias(context, YES);
     
+    // create the bezier path for the rounded corner...
     UIBezierPath *roundedCornerPath = [UIBezierPath bezierPathWithRoundedRect: rect
                                                             byRoundingCorners: _corners
                                                                   cornerRadii: CGSizeMake(_cornerRadius, _cornerRadius)];
+    
+    // and draw it!
+    // note that fill has to come before stroke if you want the border to be visible :)
     [roundedCornerPath fill];
     [roundedCornerPath stroke];
 
